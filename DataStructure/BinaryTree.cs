cfebc6e47c1a78f5e6adc21ab11161097f160c0d
pjagana <peru.jagan@gmail.com>
2017-03-14 15:51:12 -0700

    Adding comment to Binary Tree

--- DataStructure/BinaryTree.cs
+++ DataStructure/BinaryTree.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace DataStructure
 {
+    /// <summary>
+    /// Class Declaration for BinaryTree
+    /// </summary>
     class BinaryNode
     {
         public int data;
